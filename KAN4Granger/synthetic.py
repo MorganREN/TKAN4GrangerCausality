7e58f2f4bb4add0ee956f89783e46d52564db0fa
m33394mr <mohan.ren@student.manchester.ac.uk>
2024-08-05 20:00:49 +0800

    Update zexuan's demo

--- KAN4Granger/synthetic.py
+++ KAN4Granger/synthetic.py
@@ -88,34 +88,36 @@ total_length=1000
 def sigmoid(x):
         return 1 / (1 + math.exp(-x))
     
-def generate(sig_to_noise): 
+def generate_ds1(sig_to_noise): 
     np.random.seed(0)
     z = [1,1,1,1]
     x=[1,1,1,1]
     #y=[3,3,3,3]
     p = []
     n = [3,3,3,3]
-    t1 = [3,3,3,3]
+    y_cg = [3,3,3,3]
+    y_ncg = [3,3,3,3]
     for i in range(4,total_length+4):
         z.append(math.tanh(z[i-1]+np.random.normal(0,0.01)))
         p.append(z[i]**2+np.random.normal(0,0.05))
         x.append(sigmoid(z[i-2])+np.random.normal(0,0.01))
     
-        #term1 = sigmoid(z[i-4])
-        #term2 = sigmoid(x[i-2])
-        term1 = z[i-4]*z[i-3]
-        term2 = x[i-2]*x[i-1]
-        t1.append(term1+term2)
+        term1 = sigmoid(z[i-4])
+        term2 = sigmoid(x[i-2])
+        # term1 = z[i-4]*z[i-3]
+        # term2 = x[i-2]*x[i-1]
+        y_cg.append(term1 + term2 + np.random.normal(0,0.01))
+        y_ncg.append(term1 + np.random.normal(0,0.01))
         n.append(np.random.normal(0,1))
         #noise = np.random.normal(0,1)
         #alpha = (abs(term1+term2)/sig_to_noise)/abs(noise)
         #y.append(term1+term2+alpha*noise)
 
-    alpha = np.mean(t1)/sig_to_noise
-    y = np.add(t1,np.multiply(n,alpha)).tolist()
     x=x[-total_length:]
-    y=y[-total_length:]
     p=p[-total_length:]
     z=z[-total_length:]
-    df=pd.DataFrame({"x":x,"y":y,"p":p,"z":z})
+    y_cg=y_cg[-total_length:]
+    y_ncg=y_ncg[-total_length:]
+    # df=pd.DataFrame({"x":x,"y":y,"p":p,"z":z})
+    df = pd.DataFrame({'z': z, 'u': p, 'x': x, 'y_ncg': y_ncg, 'y_cg': y_cg})
     return df
